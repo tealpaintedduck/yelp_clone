deab5baf2df910959cb679d3854f72ebeb0f6f84
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-09-01 16:13:47 +0100

    can edit restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -41,4 +41,17 @@ feature 'restaurants' do
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
+
+  context 'editing restaurants' do
+    before { Restaurant.create name: 'KFC'}
+
+    scenario 'lets a user edit a restaurant' do
+      visit '/restaurants'
+      click_link "Edit KFC"
+      fill_in "Name", with: "Kentucky Fried Chicken"
+      click_button "Update Restaurant"
+      expect(page).to have_content "Kentucky Fried Chicken"
+      expect(current_path).to eq "/restaurants"
+    end
+  end
 end
